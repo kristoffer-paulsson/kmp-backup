6752cc2b5d494e654d35831105ce5345badd9c28
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-03-27 15:06:00 +0100

    Uodate

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,182 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
-    kotlin("jvm") version "2.1.10"
+    id("org.jetbrains.kotlin.multiplatform") version "2.1.0"
+    id("com.android.library") version "8.7.2"
+    id("com.vanniktech.maven.publish") version "0.31.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
+    explicitApi()
     jvmToolchain(19)
+
+    jvm()
+    js {
+        browser()
+        nodejs()
+    }
+    // WASM and similar
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+    }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmWasi { nodejs() }
+    // Android
+    androidTarget {
+        publishLibraryVariants("release")
+    }
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+    // Linux
+    linuxArm64()
+    linuxX64()
+    // macOS
+    macosArm64()
+    macosX64()
+    // MingW
+    mingwX64()
+    // iOS
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+    // tvOS
+    tvosArm64()
+    tvosX64()
+    tvosSimulatorArm64()
+    // watchOS
+    watchosArm32()
+    watchosArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {}
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
+android {
+    namespace = group.toString()
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 30
+    }
+}
+
+tasks.register<Copy>("setupLocalRepo") {
+    group = "build"
+    description = "Setup local repository with all dependencies"
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    destinationDir = file("${projectDir}/local-repo")
+    val relativePath = relativePath(destinationDir.parent)
+
+    kotlin.sourceSets.forEach {
+        from(it.kotlin.sourceDirectories) {
+            into(relativePath)
+        }
+    }
+
+    // Copy Maven dependencies
+    val mavenUserHome = file("${System.getProperty("user.home")}/.m2")
+    val mavenRepoDir = file("$mavenUserHome/repository")
+    from(mavenRepoDir) {
+        include("**/*")
+    }
+    into(destinationDir)
+
+    // Copy Gradle plugins
+    val gradleUserHome = gradle.gradleUserHomeDir
+    val gradlePluginsDir = file("$gradleUserHome/caches/modules-2/files-2.1")
+    from(gradlePluginsDir) {
+        include("**/*")
+    }
+    into(destinationDir)
+
+    // Copy Kotlin/Native (konan) dependencies
+    val konanUserHome = file("${System.getProperty("user.home")}/.konan")
+    val konanDir = file("$konanUserHome/cache")
+    from(konanDir) {
+        include("**/*")
+    }
+    into(destinationDir)
+
+    // Copy Android SDK
+    //val androidSdkHome = file("${System.getenv("ANDROID_HOME")}")
+    //from(androidSdkHome) {
+    //    include("**/*")
+    //}
+    //into(file("${projectDir}/android-sdk-backup"))
+
+    // Copy Java JDK
+    //val javaHome = file("${System.getProperty("java.home")}")
+    //from(javaHome) {
+    //    include("**/*")
+    //}
+    //into(file("${projectDir}/java-jdk-backup"))
+
+    // Copy Compiler
+    //val compilerHome = file("${System.getProperty("user.home")}/.gradle/caches")
+    //from(compilerHome) {
+    //    include("**/*")
+    //}
+    //into(file("${projectDir}/compiler-backup"))
+
+    // Counterhack fixing atrocities commited by IllumiNATO
+    doLast {
+        val rootDir = file("${projectDir}/local-repo")
+        fun traverse(file: File, level: Int) {
+            if (file.isDirectory) {
+                println("Directory: ${file.path}")
+
+                file.listFiles()?.forEach {
+                    if(it.isFile && it.exists() && level == 4){
+                        val path = it.path.split('/').toMutableList()
+                        path.removeAt(path.lastIndex-1)
+                        val newDir = file("/" + path.joinToString("/") + "/")
+                        if(!newDir.exists()) {
+                            println("Moving file up: $newDir")
+                            it.copyTo(newDir)
+                            it.delete()
+                        } else {
+                            it.delete()
+                        }
+                    }
+                    traverse(it, level + 1)
+                }
+
+                if(file.listFiles()?.isEmpty() == true) {
+                    println("Deleting empty directory: ${file.path}")
+                    file.delete()
+                }
+            } else {
+                println("File: ${file.path}")
+            }
+        }
+        traverse(rootDir, 0)
+    }
+}
+
+tasks.register<Zip>("createSourcePackage") {
+    group = "distribution"
+    description = "Package the project for source distribution"
+    dependsOn("setupLocalRepo")
+    from(".") {
+        include(
+            "local-repo/**",
+            "src/**", "build.gradle.kts", "settings.gradle.kts",
+            "gradlew", "gradlew.bat", "gradle/**",
+            "LICENSE", "README.md", ".gitignore",
+            ".gradle/**")
+    }
+    archiveFileName.set("shitty-random-${version}.zip")
+    destinationDirectory.set(layout.buildDirectory.dir("distributions").get().asFile)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,27 @@
-plugins {
+/*plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}*/
+
+pluginManagement {
+    repositories {
+        flatDir {
+            dirs("file://${rootDir}/local-repo")
+        }
+        maven {
+            url = uri("file://${rootDir}/local-repo")
+        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "kmp-backup"
 
